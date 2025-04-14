a695213b45417fb4f35d3455be787675b3987231
siddharthkush12 <siddharth5044549@gmail.com>
2025-04-14 19:29:41 +0530

    changes

--- client/src/components/Shopping/ShoppingHeader.jsx
+++ client/src/components/Shopping/ShoppingHeader.jsx
@@ -97,7 +97,7 @@ function RightMenuItems() {
 
   useEffect(()=>{
     dispatch(fetchCart(user?.id))
-    dispatch(profileList(user?.id))
+    // dispatch(profileList(user?.id))
   },[dispatch,user?.id])
   
 
