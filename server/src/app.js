75f1ef65b8684f5182283f4a0c2d74da2a092749
siddharthkush12 <siddharth5044549@gmail.com>
2025-04-14 08:13:57 +0530

    vh

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app=express();
 
 app.use(
     cors({
-        origin:['http://localhost:5173','https://e-commerce-front-eta-rosy.vercel.app/'],
+        origin:'http://localhost:5173',
         methods:['GET','POST','DELETE','PUT'],
         allowedHeaders:[
             "Content-Type",
