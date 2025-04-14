3677fb330cfbda894c5f0afa082cec4c282fec2f
siddharthkush12 <siddharth5044549@gmail.com>
2025-04-14 19:10:30 +0530

    last deploy

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ const app=express();
 
 app.use(
     cors({
-        origin:'http://localhost:5173',
+        origin:['http://localhost:5173','https://e-commerce-frontend-sqtb.onrender.com'],
         methods:['GET','POST','DELETE','PUT'],
         allowedHeaders:[
             "Content-Type",
