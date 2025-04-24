5accf1ee27d28fb13b47332e728c8cf91e31e835
siddharthkush12 <siddharth5044549@gmail.com>
2025-04-25 03:16:19 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,16 +35,11 @@ function App() {
   
   if (isLoading) {
     return (
-      // <div className="flex h-screen w-full items-center justify-center">
-      //   <div className="flex flex-col space-y-3">
-      //     <Skeleton className="h-[125px] w-[250px] rounded-xl border-r-red-400" />
-      //     <div className="space-y-2">
-      //       <Skeleton className="h-4 w-[250px] border-r-red-400" />
-      //       <Skeleton className="h-4 w-[200px] border-r-red-400" />
-      //     </div>
-      //   </div>
-      // </div>
-      <Spinner/>
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="flex flex-col space-y-3">
+          <Spinner/>  
+        </div>
+      </div>
     );
   }
 
